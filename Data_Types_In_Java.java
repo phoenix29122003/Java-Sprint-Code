17f36ced56fe07796b4bbe50c61dcaa158bcce35
phoenix29122003 <125538215+phoenix29122003@users.noreply.github.com>
2024-11-28 19:33:25 +0530

    Create Data_Types_In_Java.java

---
+++ Data_Types_In_Java.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        char character='a';
+        int integer=1;
+        byte byteData=1;
+        double decimal=1.00000;
+        float floatNum=1.23456f;
+        long longNum=12345678;
+        System.out.println("char: "+character);
+        System.out.println("integer: "+integer);
+        System.out.println("byteData: "+byteData);
+        System.out.println("decimal: "+decimal);
+        System.out.println("floatNum: "+floatNum);
+        System.out.println("longNum: "+longNum);
+    }
+}
